166ed5b6f67491884482d5caeafcd1412a76a50f
romanhan <hanmamedov.r@gmail.com>
2025-06-16 17:19:17 +0300

    test: add UserTest, small change to other test

--- src/test/java/romanhan/movie_recommendation/repository/UserRepositoryTest.java
+++ src/test/java/romanhan/movie_recommendation/repository/UserRepositoryTest.java
@@ -7,11 +7,11 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import romanhan.movie_recommendation.entity.User;
 
-@SpringBootTest
+@DataJpaTest
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
